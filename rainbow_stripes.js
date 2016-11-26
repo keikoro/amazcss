3c39db62ada0b3eb6800545607de4e1de3f14f71
Kerstin Kollmann <code@kerstinkollmann.com>
2016-11-26 20:21:16 +0100

    plus button functionality, fix animation manipulation

--- rainbow_stripes.js
+++ rainbow_stripes.js
@@ -1,6 +1,11 @@
 // execute on initial load
 document.addEventListener('DOMContentLoaded', function() {
-    addParticles(10);
+    addParticles(13);
+
+    var moar_btn = document.getElementById('moar');
+    moar_btn.addEventListener('click', function() {
+        addParticles(7);
+    });
 });
 
 // execute on browser resize
@@ -21,34 +26,47 @@ function getWindowSize() {
         height = w.innerHeight || e.clientHeight || g.clientHeight;
 
     // debug
-    // console.log("h: " + height + "px\nw: " + width + "px");
+    console.log("h: " + height + "px\nw: " + width + "px");
     return [height, width];
 }
 
-function addParticles(particleCount) {
-
+// add new particles
+function addParticles(addCount) {
     var windowSize = getWindowSize();
     var height = windowSize[0];
-    var width = windowSize[1];
-    var top = height/particleCount;
-    var left = width/particleCount;
+    var left = 10;
 
-    var dur = 4 + Math.random();
+    // vars for animations move and spin
+    var moveDur, moveDelay, spinDur;
+    // min/max duration in seconds for move, spin
+    var minMD = 8;
+    var maxMD = 14;
+    var minSD = 2;
+    var maxSD = minMD;
 
     var main = document.getElementsByClassName('maincontainer')[0];
     var div;
 
-    for (var i=1; i<=particleCount; i++) {
+    for (var i=1; i<=addCount; i++) {
+        moveDur = Math.random() * (maxMD-minMD) + minMD;
+        moveDelay = i*Math.random();
+        spinDur = Math.random() * (maxSD-minSD) + minSD;
+
         div = document.createElement("div");
         div.className = "particle";
-        div.style.setProperty('top', i*top+'px');
-        div.style.setProperty('left', i*(-left)+'px');
-        div.style.setProperty('animation-delay', i*Math.random() + 's');
-        div.style.setProperty('animation-duration', dur + 's')
+
+        div.style.setProperty('top', Math.random()*height +'px', '');
+        div.style.setProperty('left', -(i*left*Math.random())-left +'px', '');
+        console.log(div.style.left);
+
+        div.style.animation =
+            'moveParticle ' + moveDur + 's linear ' + moveDelay +
+            's infinite, rainbow ' + moveDur + 's linear ' + moveDelay +
+            's infinite, ' + 'spin ' + spinDur + 's linear infinite';
 
         main.appendChild(div);
     }
 
-    var count_p = document.getElementsByClassName('particle').length;
-    console.log("particle count: " + count_p);
+    var countP = document.getElementsByClassName('particle').length;
+    console.log("particle count: " + countP);
 }
