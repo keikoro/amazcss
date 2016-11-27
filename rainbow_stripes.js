acc777bac107ca232cde66b6f43d0da5af8d32d3
Kerstin Kollmann <code@kerstinkollmann.com>
2016-11-27 02:38:08 +0100

    func for time vals, randomising; clean up redundant vars

--- rainbow_stripes.js
+++ rainbow_stripes.js
@@ -10,9 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // execute on browser resize
 window.addEventListener('resize', function() {
-    var windowSize = getWindowSize();
-    var height = windowSize[0];
-    var width = windowSize[1];
+    // var windowSize = getWindowSize();
 });
 
 // get current browser window size
@@ -30,6 +28,24 @@ function getWindowSize() {
     return [height, width];
 }
 
+// concatenate time values for animations using seconds as unit
+function combineTimeValues(...args) {
+    var countVals = args.length;
+    var combined = '';
+
+    for (var x=0; x<countVals; x++) {
+        combined += args[x] + 's';
+        if (x<countVals-1) {
+            combined += ', ';
+        }
+    }
+    return combined;
+}
+
+function randomise(min, max) {
+    return Math.random() * (max-min) + min;
+}
+
 // add new particles
 function addParticles(addCount) {
     var windowSize = getWindowSize();
@@ -38,31 +54,23 @@ function addParticles(addCount) {
 
     // vars for animations move and spin
     var moveDur, moveDelay, spinDur;
-    // min/max duration in seconds for move, spin
-    var minMD = 8;
-    var maxMD = 14;
-    var minSD = 2;
-    var maxSD = minMD;
 
     var main = document.getElementsByClassName('maincontainer')[0];
     var div;
 
     for (var i=1; i<=addCount; i++) {
-        moveDur = Math.random() * (maxMD-minMD) + minMD;
+        // randomise(min, max) for duration in seconds for move, spin
+        moveDur = randomise(8, 14);
+        spinDur = randomise(2, 8);
         moveDelay = i*Math.random();
-        spinDur = Math.random() * (maxSD-minSD) + minSD;
 
         div = document.createElement("div");
         div.className = "particle";
 
         div.style.setProperty('top', Math.random()*height +'px', '');
         div.style.setProperty('left', -(i*left*Math.random())-left +'px', '');
-        console.log(div.style.left);
-
-        div.style.animation =
-            'moveParticle ' + moveDur + 's linear ' + moveDelay +
-            's infinite, rainbow ' + moveDur + 's linear ' + moveDelay +
-            's infinite, ' + 'spin ' + spinDur + 's linear infinite';
+        div.style.animationDuration = combineTimeValues(moveDur, moveDur, spinDur);
+        div.style.animationDelay = combineTimeValues(moveDelay, moveDelay, 0);
 
         main.appendChild(div);
     }
