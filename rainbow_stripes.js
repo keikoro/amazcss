6e88595a76b82cc99e7efc783b338852b60f522a
K Kollmann <git@k.kollmann.moe>
2016-11-27 21:56:28 +0100

    func for removing particles, better calc for left position

--- rainbow_stripes.js
+++ rainbow_stripes.js
@@ -1,16 +1,28 @@
 // execute on initial load
 document.addEventListener('DOMContentLoaded', function() {
+    var windowSize = getWindowSize();
+    var height = windowSize[0], width = windowSize[1];
+    console.log("h: " + height + "px\nw: " + width + "px");
+
     addParticles(13);
 
     var moar_btn = document.getElementById('moar');
     moar_btn.addEventListener('click', function() {
         addParticles(7);
     });
+
+    var less_btn = document.getElementById('less');
+    less_btn.addEventListener('click', function() {
+        removeParticles(10);
+    });
 });
 
 // execute on browser resize
 window.addEventListener('resize', function() {
     // var windowSize = getWindowSize();
+    // var height = windowSize[0], width = windowSize[1];
+    // console.log("h: " + height + "px\nw: " + width + "px");
+
 });
 
 // get current browser window size
@@ -24,7 +36,7 @@ function getWindowSize() {
         height = w.innerHeight || e.clientHeight || g.clientHeight;
 
     // debug
-    console.log("h: " + height + "px\nw: " + width + "px");
+    // console.log("h: " + height + "px\nw: " + width + "px");
     return [height, width];
 }
 
@@ -46,35 +58,91 @@ function randomise(min, max) {
     return Math.random() * (max-min) + min;
 }
 
+// translate current viewport min to pixels
+function getVminInPx() {
+    var windowSize = getWindowSize();
+    var height = windowSize[0];
+    var width = windowSize[0];
+    var vu;
+    if (width > height) {
+        vu = Math.floor(height/100);
+    } else {
+        vu = Math.floor(width/100);
+    }
+    return vu;
+}
+
 // add new particles
 function addParticles(addCount) {
     var windowSize = getWindowSize();
     var height = windowSize[0];
-    var left = 10;
+    var width = windowSize[0];
+    var vu = getVminInPx();
+    var left = vu*2.5;
 
     // vars for animations move and spin
     var moveDur, moveDelay, spinDur;
 
     var main = document.getElementsByClassName('maincontainer')[0];
-    var div;
+    var div, calcLeft;
 
     for (var i=1; i<=addCount; i++) {
         // randomise(min, max) for duration in seconds for move, spin
         moveDur = randomise(8, 14);
         spinDur = randomise(2, 8);
         moveDelay = i*Math.random();
+        calcLeft = -i*Math.random()*left-left;
 
-        div = document.createElement("div");
-        div.className = "particle";
+        div = document.createElement('div');
+        div.className = 'particle';
 
         div.style.setProperty('top', Math.random()*height +'px', '');
-        div.style.setProperty('left', -(i*left*Math.random())-left +'px', '');
-        div.style.animationDuration = combineTimeValues(moveDur, moveDur, spinDur);
-        div.style.animationDelay = combineTimeValues(moveDelay, moveDelay, 0);
+        div.style.setProperty('left', calcLeft +'px', '');
+        div.style.animationDuration = combineTimeValues(moveDelay+.5, moveDur, moveDur, spinDur);
+        div.style.animationDelay = combineTimeValues(0, moveDelay, moveDelay, 0);
 
         main.appendChild(div);
+
+        // change opacity to make fade in effect set in
+        window.getComputedStyle(div).opacity;
+        div.style.opacity = 1;
     }
 
-    var countP = document.getElementsByClassName('particle').length;
+    var countP = document.querySelectorAll('.particle:not([data-remove])').length;
     console.log("particle count: " + countP);
+}
+
+function removeParticles(removeCount) {
+    // convert particle nodes to array
+    var particlesNL = document.querySelectorAll('.particle:not([data-remove])');
+    var allParticles = [], toRemove = [];
+    var randomPick, elem;
+
+    allParticles.push.apply(allParticles, particlesNL);
+
+    // pick random particles to delete from all existing ones
+    // & let them fadeout gradually
+    for (var y=0; y<removeCount; y++) {
+        randomPick = Math.floor(Math.random()*allParticles.length);
+        if (particlesNL.length > 0) {
+            elem = allParticles[randomPick];
+            elem.setAttribute('data-remove', '1');
+            elem.className += ' fadeOut';
+        }
+    }
+
+    // delete elements after they have faded
+    (function(rc) {
+        setTimeout(function () {
+            toRemove.push.apply(toRemove, document.querySelectorAll('[data-remove]'));
+            var elem;
+            // check if there are still elements to delete!
+            for (var z = 0; (z < rc && z > 0); z++) {
+                elem = toRemove[z];
+                elem.parentNode.removeChild(elem);
+            }
+            var countagain = document.querySelectorAll('.particle:not([data-remove])');
+            console.log('remaining particles: ' + countagain.length);
+        }, 4000);
+    })(removeCount);
 }
